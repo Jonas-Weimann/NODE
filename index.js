7386822fffdf7451a9773076711b65f36287a06d
Jonas <jonas.weimann04@gmail.com>
2024-07-25 01:55:21 -0300

    Ingresando totales y calculando monto justo por persona

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-let total = parseInt(prompt('¿Cuánto es el importe total a pagar? Ingrese solo números'))
+let total
+let totalPersonas
 
+function ingresarTotal() {
+    total = parseInt(prompt('¿Cuánto es el importe total a pagar? Ingrese solo números'))
+    if (isNaN(total)) {
+        alert('La expresión ingresada no es un número!')
+        ingresarTotal()
+    } else console.log(total);
+}
 
+function ingresarCantidadDePersonas() {
+    totalPersonas = parseInt(prompt('¿Entre cuántas personas se dividirá la cuenta? Ingrese solo números'))
+    if (isNaN(totalPersonas)) {
+        alert('La expresión ingresada no es un número!')
+        ingresarCantidadDePersonas()
+    } else console.log(totalPersonas);
+}
+
+ingresarTotal();
+ingresarCantidadDePersonas();
+
+let montoJusto = total / totalPersonas;
+alert('El monto justo por persona es de ' + montoJusto)
+console.log(montoJusto)
