16ca1bc16d3308fd9802dac2566f3595eaef78c0
Jonas <jonas.weimann04@gmail.com>
2024-07-25 03:09:01 -0300

    Haciendo array de personas

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 // //Iniciando las variables total y total de personas
-// let total
-// let totalPersonas
+let total
+let totalPersonas
 
-// function ingresarTotal() {
-//     total = parseInt(prompt('¿Cuánto es el importe total a pagar? Ingrese solo números'))
-//     if (isNaN(total)) {
-//         alert('La expresión ingresada no es un número!')
-//         ingresarTotal()
-//     } else console.log(total);
-// }
+function ingresarTotal() {
+    total = parseInt(prompt('¿Cuánto es el importe total a pagar? Ingrese solo números'))
+    if (isNaN(total)) {
+        alert('La expresión ingresada no es un número!')
+        ingresarTotal()
+    } else console.log(total);
+}
 
 function ingresarCantidadDePersonas() {
     totalPersonas = parseInt(prompt('¿Entre cuántas personas se dividirá la cuenta? Ingrese solo números'))
@@ -18,14 +18,14 @@ function ingresarCantidadDePersonas() {
     } else console.log(totalPersonas);
 }
 
-// ingresarTotal();
+ingresarTotal();
 ingresarCantidadDePersonas();
 
 
 //Calculando el monto justo por persona
-// let montoJusto = total / totalPersonas;
-// alert('El monto justo por persona es de ' + montoJusto)
-// console.log(montoJusto)
+let montoJusto = total / totalPersonas;
+alert('El monto justo por persona es de ' + montoJusto)
+console.log(montoJusto)
 
 //Inicializando array de personas
 let personas = []
@@ -51,5 +51,7 @@ for (let i= 0; i < totalPersonas; i++) {
         nombre: ingresarNombre([i + 1]),
         aporte: ingresarAporte(),
     }
-    console.log(persona);
+    personas.push(persona)
 }
+
+console.log(personas)
